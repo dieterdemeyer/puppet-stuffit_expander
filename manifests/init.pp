0d7cabcc5b1b1a5aac3126adf510b86ca9edff80
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-01 20:19:50 +0100

    Template for installing StuffIt Expander

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install StuffIt Expander to /Applications 
+#
+# Examples
+#
+#  include stuffit_expander
+class stuffit_expander {
+  package { '':
+    provider => '',
+    source   => '',
+  }
+}
