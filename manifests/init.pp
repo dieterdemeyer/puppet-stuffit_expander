66cd213d2ad2142c120630c3088656d5a210a84a
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-02 18:25:13 +0100

    Implementation for installing StuffIt Expander

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #  include stuffit_expander
 class stuffit_expander {
-  package { '':
-    provider => '',
-    source   => '',
+  package { 'StuffIt Expander':
+    provider => 'appdmg',
+    source   => 'https://yum.cegeka.be/osx_packages/StuffIt-Expander-15.0.7.dmg',
   }
 }
