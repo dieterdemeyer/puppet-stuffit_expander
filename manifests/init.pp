b446249e4431149ab04a0acdef9b0fc6a515d846
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 17:52:35 +0200

    Change download location

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
-# Public: Install StuffIt Expander to /Applications 
+# Public: Install StuffIt Expander to /Applications
 #
 # Examples
 #
 #  include stuffit_expander
 class stuffit_expander {
+
   package { 'StuffIt Expander':
     provider => 'appdmg',
-    source   => 'https://yum.cegeka.be/osx_packages/StuffIt-Expander-15.0.7.dmg',
+    source   => 'https://dl.dropboxusercontent.com/s/d7mpnqzx2q7aqam/StuffIt-Expander-15.0.7.dmg',
   }
+
 }
